df826cc355deba45f3dfa9393f7ba30b339629e3
dvub <dvubdevs@gmail.com>
2024-04-16 21:09:20 -0700

    styling, content changes

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -7,9 +7,29 @@ export default function Page() {
 				</h1>
 				{/* TODO: possible subheader here */}
 			</div>
-			<div className='text-xl '>
-				Online Privacy Awareness was started by 2 students for a project
-				about government
+			<div className='text-xl lg:max-w-[50%] md:max-w-[75%] max-w-[90%]'>
+				<p className='my-3'>
+					Online Privacy Awareness is a project started by 2 students
+					for a goverment class.
+				</p>
+				<p className='my-3'>
+					This project aims to spread awareness on the many issues of
+					online privacy and personal data in the digital age.
+					Furthermore, (for American readers) this project seeks to
+					educate people on online privacy in relation to the Fourth
+					Amendment, such as how the government has been and is
+					currently able to access your data, as well as the
+					constitutionality of these methods.
+				</p>
+				<p className='my-3'>
+					<b>
+						The goal of this project is to encourage as many as
+						possible to not only be more mindful of their online
+						activity and digital footprint, but also to choose
+						privacy-focused tech alternatives.
+					</b>{' '}
+					We wish to achieve these goals in a free and accessible way.
+				</p>
 			</div>
 		</main>
 	);
--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 import NavBar from '@/components/navbar';
 import { Button } from '@/components/ui/button';
 import { ChevronDown } from 'lucide-react';
+import Link from 'next/link';
 import { BrowserView, isMobile, MobileView } from 'react-device-detect';
 
 export default function Home() {
@@ -15,27 +16,12 @@ export default function Home() {
 						</h1>
 						{/* TODO: possible subheader here */}
 					</div>
-					<div className='text-xl max-w-[90%]'>
+					<div className='text-xl lg:max-w-[50%] md:max-w-[75%] max-w-[90%]'>
 						{isMobile && (
-							<>
-								<p>
-									Spreading awareness on the many issues of
-									online privacy and personal data in the
-									digital age.
-								</p>
-								<div className='BUTTON-WRAPPER flex justify-center'>
-									<Button
-										variant='outline'
-										className='my-5'
-										size='lg'
-										onClick={() => {
-											console.log('hi :)');
-										}}
-									>
-										More <ChevronDown className='w-3 h-3' />
-									</Button>
-								</div>
-							</>
+							<p>
+								Spreading awareness on issues of online privacy
+								and personal data in the digital age.
+							</p>
 						)}
 						{!isMobile && (
 							<p>
@@ -47,19 +33,18 @@ export default function Home() {
 								Amendment, such as how the government has been
 								and is currently able to access your data, as
 								well as the constitutionality of these methods.
-								<b>
-									The goal of this project is to encourage as
-									many as possible to not only be more mindful
-									of their online activity and digital
-									footprint, but also to choose
-									privacy-focused tech alternatives.
-								</b>{' '}
-								We wish to achieve these goals in a free,
-								accessible, and open-source way. If you wish to
-								contribute to this project, see this page
-								(todo).
 							</p>
 						)}
+						<div className='BUTTON-WRAPPER flex justify-center'>
+							<Button
+								asChild
+								variant='outline'
+								className='my-5'
+								size='lg'
+							>
+								<Link href='/about'> More</Link>
+							</Button>
+						</div>
 					</div>
 				</div>
 			</div>
