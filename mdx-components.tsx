c18f649a1473bd1daf36158a55657c75feed5410
dvub <dvubdevs@gmail.com>
2024-04-22 11:13:52 -0700

    work on styling mdx content

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,7 +1,17 @@
 import type { MDXComponents } from 'mdx/types';
+import Link from 'next/link';
+import { Children } from 'react';
+import { Url } from 'url';
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
 	return {
+		h1: ({ children }) => <h1 className='text-3xl'>{children}</h1>,
+		ul: ({ children }) => <ul className='list-disc text-xs'>{children}</ul>,
+		a: ({ children, href }) => (
+			<Link href={href} className='underline text-red-500'>
+				{children}
+			</Link>
+		),
 		...components,
 	};
 }
